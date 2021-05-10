4eeb05664b9fe1a0d2b381038bd1c2500863c412
Larisa Popa <larisapopa4@gmail.com>
2021-05-10 11:30:53 +0300

    webapp

---
+++ Week3/appdg2/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, render_template, send_file, redirect, url_for
+from werkzeug.utils import secure_filename
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/download-template', methods=['POST'])
+def download_csv_template():
+    return send_file('template.csv',
+                     mimetype='text/csv',
+                     attachment_filename='template.csv',
+                     as_attachment=True)
+
+#every upload file will be saved with this name
+global uploaded_doc_name
+uploaded_doc_name = 'downloaded.csv'
+
+@app.route('/upload-dataset', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(uploaded_doc_name))
+        return redirect(url_for('uploaded_successfully'))
+
+@app.route('/uploaded-successfully')
+def uploaded_successfully():
+    return render_template('redirect_upload_success.html')
+
+# save predicted file as csv and update name
+global predicted_file_name_csv
+predicted_file_name_csv = 'test_prediction.csv'
+
+
+@app.route('/download-prediction', methods=['POST'])
+def download_csv_predicted():
+    return send_file(predicted_file_name_csv,
+                     mimetype='text/csv',
+                     attachment_filename=predicted_file_name_csv,
+                     as_attachment=True)
+
+if  __name__== '__main__':
+    app.run(port=8082, debug=True)
